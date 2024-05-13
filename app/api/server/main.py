7c19b3c6e45f3262ff9780f3a44ea4134b1dfcd1
taraslysun <taraslysun2@gmail.com>
2024-05-14 00:20:49 +0300

    added elasticsearch

--- app/api/server/main.py
+++ app/api/server/main.py
@@ -1,24 +1,27 @@
-"Main server module"
-
-from elasticsearch import Elasticsearch
-from flask_cors import CORS
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+from elasticsearch import Elasticsearch
 import subprocess
 import shlex
 
-CLOUD_ID = ""
-API_KEY = ""
-
-es = Elasticsearch(cloud_id=CLOUD_ID, api_key=API_KEY, node_class='requests')
 
 app = Flask(__name__)
 CORS(app)
 
 
+@app.route('/api/api_credentials', methods=['POST'])
+def api_credentials():
+    data = request.json
+    CLOUD_ID = data['cloud_id']
+    API_KEY = data['api_key']
+    global es
+    es = Elasticsearch(cloud_id=CLOUD_ID, api_key=API_KEY, node_class='requests')
+    print(CLOUD_ID, API_KEY)
+    return jsonify({"message": "API credentials set"})
+
 
 @app.route('/api/search', methods=['POST', 'GET'])
 def search():
-    "Seach route"
     query = request.json.get('query')
     res = es.search(index="test", body={
         "query": {
@@ -30,11 +33,7 @@ def search():
             },
         },
     })
-    hits = res['hits']['hits']
-    return jsonify(hits)
-
-app = Flask(__name__)
-CORS(app)
+    return jsonify(res['hits']['hits'])
 
 @app.route('/api/execute_ssh', methods=['POST'])
 def execute_ssh():
@@ -42,25 +41,24 @@ def execute_ssh():
     ip = data['ip']
     username = data['username']
     password = data['password']
-
     path_to_crawler = data['path_to_crawler']
     path_to_task_manager = data['path_to_task_manager']
     is_host = data['is_host']
-
-    id = data['id']
     host_ip = data['host_ip']
     worker_num = data['worker_num']
 
-    print(ip, username, password, path_to_crawler, is_host, path_to_task_manager)
-
-    cd_crawler = f"cd {path_to_crawler}"
     if is_host:
-        cd_task_manager = f"cd {path_to_task_manager}"
-        run_cr = f"go run main.go worker {host_ip} {id}"
-    else:    
-        run_cr = f"go run main.go host {host_ip} {worker_num}"
-    command = f"sshpass -p {password} ssh -o StrictHostKeyChecking=no {username}@{ip} '{cd_crawler}; {run_cr}'"
+        tm_commands = f"cd {path_to_task_manager} && nohup go run . &"
+        cr_commands = f"cd {path_to_crawler} && go run main.go master {host_ip} {worker_num}"
+        full_command = f"{tm_commands} {cr_commands}"
+    else:
+        full_command = f"cd {path_to_crawler} && go run main.go worker {host_ip}"
 
+    ssh_command = f"sshpass -p {shlex.quote(password)} ssh -o StrictHostKeyChecking=no \
+{shlex.quote(username)}@{shlex.quote(ip)} '{full_command}'"
+
+    process = subprocess.Popen(ssh_command, shell=True)
+    return jsonify({"message": "SSH command executed", "pid": process.pid})
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
